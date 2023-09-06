4d9d77d8c35d4aaa2a1422bca71ec8ffc27bfa5c
Vika <liusikova.vika@yandex.ru>
2023-09-06 21:35:12 +0300

    level-2: NavTab, Landing, Promo

---
+++ src/components/App/App.js
@@ -0,0 +1,12 @@
+import Landing from "../Landing/Landing";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="content">
+      <Landing />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Landing/Landing.js
@@ -0,0 +1,13 @@
+import Promo from "../Promo/Promo";
+import NavTab from "../NavTab/NavTab";
+
+function Landing() {
+  return (
+    <main>
+      <Promo />
+      <NavTab />
+    </main>
+  );
+}
+
+export default Landing;

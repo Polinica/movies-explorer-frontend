f5d78055542200bc628b2009e8a8488548be35fb
Vika <liusikova.vika@yandex.ru>
2023-09-07 15:37:11 +0300

    level-2: add AboutProject, Techs, AboutMe

--- src/components/Landing/Landing.js
+++ src/components/Landing/Landing.js
@@ -1,11 +1,22 @@
 import Promo from "../Promo/Promo";
 import NavTab from "../NavTab/NavTab";
+import Article from "../Article/Article";
 
 function Landing() {
   return (
     <main>
       <Promo />
       <NavTab />
+      <Article title="О&nbsp;проекте" navId="about-project">
+        <div>Описание диплома</div>
+      </Article>
+      <Article title="Технологии" navId="techs" isThemed={true}>
+        <div>7 технологий</div>
+      </Article>
+      <Article title="Студент" navId="about-me">
+        <div>Описание</div>
+        <div>Портфолио</div>
+      </Article>
     </main>
   );
 }

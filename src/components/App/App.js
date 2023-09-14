cd71554c5a3a9396bb90903fd1339fee1ed97703
Vika <liusikova.vika@yandex.ru>
2023-09-14 16:43:39 +0300

    level-2: add /movies

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,10 +1,15 @@
+import { Route, Routes } from "react-router-dom";
 import Landing from "../Landing/Landing";
+import Movies from "../Movies/Movies";
 import "./App.css";
 
 function App() {
   return (
     <div className="content">
-      <Landing />
+      <Routes>
+        <Route path="/" element={<Landing />} />
+        <Route path="/movies" element={<Movies />} />
+      </Routes>
     </div>
   );
 }
--- src/components/Landing/Landing.js
+++ src/components/Landing/Landing.js
@@ -6,7 +6,6 @@ import AboutProject from "../AboutProject/AboutProject";
 import Techs from "../Techs/Techs";
 import Footer from "../Footer/Footer";
 import Header from "../Header/Header";
-import Navigation from "../Navigation/Navigation";
 
 function Landing() {
   return (

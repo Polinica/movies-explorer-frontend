4829ea83e27dd5646ad1dc9531e1b46f6765b363
Vika <liusikova.vika@yandex.ru>
2023-09-16 15:30:06 +0300

    level-2: cardlist st

--- src/components/movies/MoviesCard/MoviesCard.js
+++ src/components/movies/MoviesCard/MoviesCard.js
@@ -6,9 +6,9 @@ import CardButton from "../CardButton/CardButton";
 function MoviesCard() {
   return (
     <li className="movie-card">
+      <img src={photo} alt="" className="movie-card__thumbnail" />
       <h3 className="movie-card__name">33 слова о дизайне</h3>
       <p className="movie-card__duration">1 час 42 минуты</p>
-      <img src={photo} alt="" className="movie-card__thumbnail" />
       <CardButton className="movie-card__button" type="delete" />
     </li>
   );

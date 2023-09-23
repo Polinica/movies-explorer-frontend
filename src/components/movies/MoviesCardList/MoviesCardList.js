92bd78b5f72eb3f67b0ea2eeb9287748de2229a5
Vika <liusikova.vika@yandex.ru>
2023-09-23 19:00:31 +0300

    level-3: add initial movies to api

--- src/components/movies/MoviesCardList/MoviesCardList.js
+++ src/components/movies/MoviesCardList/MoviesCardList.js
@@ -2,17 +2,22 @@ import React from "react";
 
 import MoviesCard from "../MoviesCard/MoviesCard";
 import "./MoviesCardList.css";
+import moviesApi from "../../../utils/MoviesApi";
 
 import testData from "../../../utils/testData";
 
 function MoviesCardList({ type }) {
   const [movies, setMovies] = React.useState([]);
-  function testGetMovies() {
-    setMovies(testData);
+  // function testGetMovies() {
+  //   setMovies(testData);
+  // }
+  async function getMovies() {
+    const movies = await moviesApi.getMovies();
+    setMovies(movies);
   }
 
   React.useEffect(() => {
-    testGetMovies();
+    getMovies();
   }, []);
   return (
     <ul className="movie-card-list section" aria-label="Список фильмов">

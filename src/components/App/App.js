e667688775f736940682c4ec6387ce406fc0748d
Vika <liusikova.vika@yandex.ru>
2023-09-25 10:14:47 +0300

    level-3: add logOut

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,7 +1,7 @@
 import { Route, Routes, useNavigate } from "react-router-dom";
 import Landing from "../Landing/Landing";
 import Movies from "../movies/Movies/Movies";
-import ProfilePage from "../ProfilePage/ProfilePage";
+// import ProfilePage from "../ProfilePage/ProfilePage";
 import SavedMovies from "../movies/SavedMovies/SavedMovies";
 import Auth from "../Auth/Auth";
 import Page404 from "../Page404/Page404";
@@ -11,18 +11,45 @@ import Login from "../user/Login/Login";
 import React from "react";
 import CurrentUserContext from "../../contexts/CurrentUserContext";
 import mainApi from "../../utils/MainApi";
+import Profile from "../Profile/Profile";
 
 function App() {
   const [currentUser, setCurrentUser] = React.useState(null);
 
   const navigate = useNavigate();
 
+  // Авторизация при открытии страницы
+  React.useEffect(() => {
+    const token = localStorage.getItem("token");
+    if (token) {
+      mainApi
+        .checkToken(token)
+        .then((res) => {
+          mainApi.setToken(token);
+          setCurrentUser(res);
+        })
+        .catch((err) => {
+          localStorage.removeItem("token");
+          setCurrentUser(null);
+          console.error(err);
+        });
+    }
+  }, [navigate]);
+
   async function handleLogin({ token }) {
     if (token) {
       localStorage.setItem("token", token);
       mainApi.setToken(token);
+      const user = await mainApi.getUserInfo();
+      setCurrentUser(user);
+      navigate("/movies");
     }
-    navigate("/movies");
+  }
+
+  function handleLogOut() {
+    localStorage.removeItem("token");
+    setCurrentUser(null);
+    navigate("/");
   }
 
   return (
@@ -32,7 +59,10 @@ function App() {
           <Route path="/" element={<Landing />} />
           <Route path="/movies" element={<Movies />} />
           <Route path="/saved-movies" element={<SavedMovies />} />
-          <Route path="/profile" element={<ProfilePage />} />
+          <Route
+            path="/profile"
+            element={<Profile onLogout={handleLogOut} />}
+          />
           <Route
             path="/signup"
             element={<Register onRegister={handleLogin} />}
--- src/components/Profile/Profile.js
+++ src/components/Profile/Profile.js
@@ -1,8 +1,14 @@
 import React from "react";
+import CurrentUserContext from "../../contexts/CurrentUserContext";
+import useForm from "../../utils/hooks/useFormWithValidation";
+import Header from "../Header/Header";
 import "./Profile.css";
 import SubmitButton from "../user/SubmitButton/SubmitButton";
 
-function Profile() {
+function Profile({ onLogout }) {
+  const currentUser = React.useContext(CurrentUserContext);
+
+  const [values, errors, isValid, handleChange] = useForm();
   const [isInEditMode, setIsInEditMode] = React.useState(true);
 
   function switchEditMode(evt) {
@@ -11,66 +17,78 @@ function Profile() {
   }
 
   return (
-    <main className="profile content__stretched-element">
-      <div className="profile__container">
-        <h1 className="profile__title">Привет, Виталий!</h1>
-        <form className="profile__form">
-          <label className="profile__input-container">
-            <span className="profile__input-label">Имя</span>
-            <input
-              type="text"
-              className="profile__input"
-              name="name"
-              minLength="2"
-              maxLength="30"
-              required={true}
-              {...(!isInEditMode ? { disabled: true } : {})}
-            />
-          </label>
-          <label className="profile__input-container">
-            <span className="profile__input-label">E-mail</span>
-            <input
-              type="email"
-              className="profile__input"
-              name="email"
-              required={true}
-              {...(!isInEditMode ? { disabled: true } : {})}
-            />
-          </label>
+    <>
+      <Header />
+      <main className="profile content__stretched-element">
+        <div className="profile__container">
+          <h1 className="profile__title">{`Привет, ${
+            currentUser?.name ?? "Виталий"
+          }!`}</h1>
+          <form className="profile__form">
+            <label className="profile__input-container">
+              <span className="profile__input-label">Имя</span>
+              <input
+                type="text"
+                className="profile__input"
+                name="name"
+                minLength="2"
+                maxLength="30"
+                required={true}
+                value={values.name ?? currentUser?.name ?? ""}
+                onChange={handleChange}
+                {...(!isInEditMode ? { disabled: true } : {})}
+              />
+            </label>
+            <label className="profile__input-container">
+              <span className="profile__input-label">E-mail</span>
+              <input
+                type="email"
+                className="profile__input"
+                name="email"
+                required={true}
+                value={values.email ?? currentUser?.email ?? ""}
+                onChange={handleChange}
+                {...(!isInEditMode ? { disabled: true } : {})}
+              />
+            </label>
 
-          {isInEditMode && (
-            <>
-              <p className="profile__error-message">
-                При обновлении профиля произошла ошибка.
-              </p>
-              <SubmitButton
-                //className="input__submit-button"
-                onClick={switchEditMode}
-                // type="submit"
-                title="Сохранить"
-              >
-                Сохранить
-              </SubmitButton>
-            </>
+            {isInEditMode && (
+              <>
+                <p className="profile__error-message">
+                  При обновлении профиля произошла ошибка.
+                </p>
+                <SubmitButton
+                  //className="input__submit-button"
+                  onClick={switchEditMode}
+                  // type="submit"
+                  title="Сохранить"
+                >
+                  Сохранить
+                </SubmitButton>
+              </>
+            )}
+          </form>
+
+          {!isInEditMode && (
+            <ul className="profile__links">
+              <li className="profile__links-item">
+                <button className="profile__link" onClick={switchEditMode}>
+                  Редактировать
+                </button>
+              </li>
+              <li className="profile__links-item">
+                <button
+                  className="profile__link profile__link_type_logout"
+                  onClick={onLogout}
+                >
+                  Выйти из аккаунта
+                </button>
+              </li>
+            </ul>
           )}
-        </form>
-
-        {!isInEditMode && (
-          <ul className="profile__links">
-            <li className="profile__links-item">
-              <button className="profile__link" onClick={switchEditMode}>
-                Редактировать
-              </button>
-            </li>
-            <li className="profile__links-item">
-              <button className="profile__link profile__link_type_logout">
-                Выйти из аккаунта
-              </button>
-            </li>
-          </ul>
-        )}
-      </div>
-    </main>
+        </div>
+      </main>
+    </>
   );
 }
 
